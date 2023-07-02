a6e6e6b98da5f91a0b16acbc4e7dd47adddbe710
Pro_Gamer_711 <81502910+Pro-Gamer-711-YT@users.noreply.github.com>
2023-07-02 10:52:04 +0000

    Structure Commited

---
+++ src/structures/CommandCategory.js
@@ -0,0 +1,22 @@
+module.exports = {
+    BOOKS: {
+      name: "Books",
+      image: "https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/book-bookmark-icon.png",
+      emoji: "📚",
+    },
+    FUN: {
+      name: "Fun",
+      image: "https://icons.iconarchive.com/icons/flameia/aqua-smiles/128/make-fun-icon.png",
+      emoji: "😂",
+    },
+    OWNER: {
+      name: "Owner",
+      image: "https://www.pinclipart.com/picdir/middle/531-5318253_web-designing-icon-png-clipart.png",
+      emoji: "🤴",
+    },
+    SOCIAL: {
+      name: "Social",
+      image: "https://icons.iconarchive.com/icons/dryicons/aesthetica-2/128/community-users-icon.png",
+      emoji: "🫂",
+    },
+  };
